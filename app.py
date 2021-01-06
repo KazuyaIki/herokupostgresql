f7c6014adcbe58f2beda82a5a04e90acec4d9950
Kazuya Iki <kzyikiggl@gmail.com>
2021-01-06 21:15:08 +0900

    second commit

--- app.py
+++ app.py
@@ -5,4 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     message = 'heroku postgresql test'
-    return render_template('index.html')
+    return render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
