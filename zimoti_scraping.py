27ed29f0382418f998484d8fc69a6d1885c9bfc3
Kazuya Iki <kzyikiggl@gmail.com>
2021-01-08 23:28:58 +0900

    jimo6

--- zimoti_scraping.py
+++ zimoti_scraping.py
@@ -5,23 +5,20 @@ import csv
 import sys
 import urllib
 import time
+import lxml
 
 def get_date(date, subdate=None):
     if subdate is not None:
         date = date - subdate
     return (str(date.month) + "m" + str(date.day) + "d")
 
-
 def add_csv_rows(writer, category, keyword, date_from, date_to):
     keyword = urllib.parse.quote(keyword)
     r = requests.get("https://jmty.jp/all/" + category + "?keyword=" + keyword)
-    # HTMLソースを取得
-    soup = BeautifulSoup(r.text, "html")
+    soup = BeautifulSoup(r.text, "HTML")
 
-    # ソース内の"li"をすべて取得
     lis = soup.find_all(class_="p-articles-list-item")
 
-    # もし、更新日がdate_toであれば、その投稿のタイトル、URL、更新日を取得
     for li in lis:
         div = li.find("div", class_="p-item-additional-info").find("div", class_="u-margin-xs-b")
         post_date = div.text
@@ -37,16 +34,11 @@ def add_csv_rows(writer, category, keyword, date_from, date_to):
             pass
 
 def export_csv(args):
-    # コマンド例： python zimoti_scraping.py 2(n日前から)
     from_days = 1 if len(args) < 2 else int(args[1])
     date_from = get_date(datetime.date.today(), datetime.timedelta(days=from_days))
-
-    # 現在の日付を取得
     date_to = get_date(datetime.date.today())
-    # csvをセット
     csv_path = 'csv/' + date_to + ".csv"
     categories = ["com", "coop"]
-    # keywords = "プログラミング,Laravel,php,python,パソコン"
     keywords = "html,css,javascript,プログラミング,Laravel,php,python,PHP,Python"
     with open(csv_path, "w") as f:
         writer = csv.writer(f)
